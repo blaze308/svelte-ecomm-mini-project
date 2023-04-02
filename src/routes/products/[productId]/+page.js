b16868a25ce709bf2c5e63c8383eb5321f8ad505
blaze_308 <jaimepinkrah@gmail.com>
2023-04-02 18:09:38 +0000

    Update +page.js

--- src/routes/products/[productId]/+page.js
+++ src/routes/products/[productId]/+page.js
@@ -2,7 +2,7 @@
 export const load = ({ fetch, params }) => {
     console.log(params);
 
-    export const fetchProducts = async (id) => {
+    const fetchProducts = async (id) => {
         const res = await fetch(`http://localhost:3000/products/${id}`)
         const data = await res.json();
         return data;
